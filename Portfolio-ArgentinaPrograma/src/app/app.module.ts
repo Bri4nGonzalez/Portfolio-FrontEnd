d81f06f9978b9eccee001b6c530b2b4d29c375c4
Brian Gonzalez <briangonzales460@gmail.com>
2022-06-03 17:33:33 -0300

    se agrego boostrap y la extructuras de nav,header y acercade

--- Portfolio-ArgentinaPrograma/src/app/app.module.ts
+++ Portfolio-ArgentinaPrograma/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,

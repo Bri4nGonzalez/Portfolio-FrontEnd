741e869783468f714209363f7a1ce456ae5a4396
Bri4nGonzalez <briangonzales460@gmail.com>
2022-08-14 17:18:35 -0300

    todas la secciones completadas

--- Portfolio-ArgentinaPrograma/src/app/app.module.ts
+++ Portfolio-ArgentinaPrograma/src/app/app.module.ts
@@ -8,6 +8,10 @@ import { NavComponent } from './components/nav/nav.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { ExperienciaEducacionComponent } from './components/experiencia-educacion/experiencia-educacion.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
@@ -16,11 +20,15 @@ import { ExperienciaEducacionComponent } from './components/experiencia-educacio
     NavComponent,
     AcercaDeComponent,
     HeroComponent,
-    ExperienciaEducacionComponent
+    ExperienciaEducacionComponent,
+    HabilidadesComponent,
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
